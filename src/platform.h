7ce354d568d6e1f83ef11dcbe79393047b7b56ff
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-03-14 09:20:39 +0100

    Format `platform.h`

--- src/platform.h
+++ src/platform.h
@@ -9,33 +9,33 @@
 #include "../include/fs.h"
 
 int
-fs__trim (uv_file file, int64_t offset, size_t length);
+fs__trim(uv_file file, int64_t offset, size_t length);
 
 int
-fs__sparse (uv_file file);
+fs__sparse(uv_file file);
 
 int
-fs__lock (uv_file file, int64_t offset, size_t length, bool shared);
+fs__lock(uv_file file, int64_t offset, size_t length, bool shared);
 
 int
-fs__try_lock (uv_file file, int64_t offset, size_t length, bool shared);
+fs__try_lock(uv_file file, int64_t offset, size_t length, bool shared);
 
 int
-fs__downgrade_lock (uv_file file, int64_t offset, size_t length);
+fs__downgrade_lock(uv_file file, int64_t offset, size_t length);
 
 int
-fs__try_downgrade_lock (uv_file file, int64_t offset, size_t length);
+fs__try_downgrade_lock(uv_file file, int64_t offset, size_t length);
 
 int
-fs__upgrade_lock (uv_file file, int64_t offset, size_t length);
+fs__upgrade_lock(uv_file file, int64_t offset, size_t length);
 
 int
-fs__try_upgrade_lock (uv_file file, int64_t offset, size_t length);
+fs__try_upgrade_lock(uv_file file, int64_t offset, size_t length);
 
 int
-fs__unlock (uv_file file, int64_t offset, size_t length);
+fs__unlock(uv_file file, int64_t offset, size_t length);
 
 int
-fs__swap (const char *from, const char *to);
+fs__swap(const char *from, const char *to);
 
 #endif // FS_PLATFORM_H
